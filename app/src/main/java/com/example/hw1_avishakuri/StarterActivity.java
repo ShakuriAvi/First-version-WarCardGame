a75fd5298b5a30a465f7ec95128cad0ce617111a
ShakuriAvi <shakuri.avi96@gmail.com>
2020-11-22 21:04:48 +0200

    add class game and put there the logical of game

---
+++ app/src/main/java/com/example/hw1_avishakuri/StarterActivity.java
@@ -0,0 +1,14 @@
+package com.example.hw1_avishakuri;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StarterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
